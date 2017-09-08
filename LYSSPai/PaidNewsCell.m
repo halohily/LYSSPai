3184f3bfa6fa346019d26eb2f7acebb9c4bd4dae
刘毅 <liuyi@quanquan6.com>
2017-09-08 11:24:14 +0800

    optimize homepage cell(pre calculate&remove autolayout)

--- LYSSPai/PaidNewsCell.m
+++ LYSSPai/PaidNewsCell.m
@@ -27,26 +27,18 @@
 
 - (void)setupUI
 {
+    float cellWidth = LYScreenWidth * 0.55;
+    float cellHeight = LYScreenWidth * 0.7;
     UILabel *cellTitle = [[UILabel alloc] init];
     [self.contentView addSubview:cellTitle];
-    [cellTitle mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.contentView.mas_left).with.offset(25.0);
-        make.top.mas_equalTo(self.contentView.mas_top).with.offset(10.0);
-        make.width.mas_equalTo(100.0);
-        make.height.mas_equalTo(18.0);
-    }];
+    cellTitle.frame = CGRectMake(25, 10, 100, 18);
     cellTitle.font = [UIFont boldSystemFontOfSize:16.0];
     cellTitle.textAlignment = NSTextAlignmentLeft;
     cellTitle.text = @"付费栏目";
     
     UIButton *more = [[UIButton alloc] init];
     [self.contentView addSubview:more];
-    [more mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.contentView.mas_top).with.offset(11.0);
-        make.right.mas_equalTo(self.contentView.mas_right).with.offset(-25.0);
-        make.width.mas_equalTo(40.0);
-        make.height.mas_equalTo(16.0);
-    }];
+    more.frame = CGRectMake(LYScreenWidth - 65, 11, 40, 16);
     [more setTitle:@"更多" forState:UIControlStateNormal];
     [more setTitleColor:UIColor(170, 170, 170) forState:UIControlStateNormal];
     more.titleLabel.font = [UIFont systemFontOfSize:14.0];
@@ -54,19 +46,14 @@
     
     UIScrollView *backScrollView = [[UIScrollView alloc] init];
     [self.contentView addSubview:backScrollView];
-    [backScrollView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.contentView.mas_left).with.offset(0);
-        make.top.mas_equalTo(cellTitle.mas_bottom).with.offset(15.0);
-        make.right.mas_equalTo(self.contentView.mas_right).with.offset(0);
-        make.height.mas_equalTo(LYScreenWidth * 0.7);
-    }];
+    backScrollView.frame = CGRectMake(0, 43, LYScreenWidth, cellHeight);
     backScrollView.backgroundColor = [UIColor whiteColor];
     backScrollView.showsHorizontalScrollIndicator = NO;
-    backScrollView.contentSize = CGSizeMake(35 + self.model.PaidNewsData.count * (LYScreenWidth * 0.55 + 15), LYScreenWidth * 0.7);
+    backScrollView.contentSize = CGSizeMake(35 + self.model.PaidNewsData.count * (cellWidth + 15), cellHeight);
     
     for (int i = 0; i < self.model.PaidNewsData.count; i++)
     {
-        UIImageView *paidNewsView = [[UIImageView alloc] initWithFrame:CGRectMake(25 + (LYScreenWidth * 0.55 + 15) * i, 0, LYScreenWidth * 0.55, LYScreenWidth * 0.7)];
+        UIImageView *paidNewsView = [[UIImageView alloc] initWithFrame:CGRectMake(25 + (cellWidth + 15) * i, 0, cellWidth, cellHeight)];
         paidNewsView.layer.cornerRadius = 5.0;
         paidNewsView.layer.masksToBounds = YES;
         paidNewsView.tag = i;
@@ -74,39 +61,25 @@
         
         UILabel *paidTitle = [[UILabel alloc] init];
         [paidNewsView addSubview:paidTitle];
-        paidTitle.preferredMaxLayoutWidth = (LYScreenWidth * 0.55 - 30.0);
-        [paidTitle setContentHuggingPriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+        paidTitle.text = self.model.PaidNewsData[i][@"title"];
         paidTitle.numberOfLines = 0;
-        [paidTitle mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.mas_equalTo(paidNewsView.mas_left).with.offset(15.0);
-            make.right.mas_equalTo(paidNewsView.mas_right).with.offset(-15.0);
-            make.top.mas_equalTo(paidNewsView.mas_top).with.offset(50.0);
-        }];
+        NSDictionary *attribute = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:20]};
+        CGSize labelSize = [paidTitle.text boundingRectWithSize:CGSizeMake(cellWidth - 30, 50) options: NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:attribute context:nil].size;
         paidTitle.textAlignment = NSTextAlignmentLeft;
         paidTitle.font = [UIFont boldSystemFontOfSize:20.0];
+        paidTitle.frame = CGRectMake(15, 50, cellWidth - 30, labelSize.height);
         paidTitle.textColor = [UIColor whiteColor];
-        paidTitle.text = self.model.PaidNewsData[i][@"title"];
         
         UIImageView *avator = [[UIImageView alloc] init];
         [paidNewsView addSubview:avator];
-        [avator mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.mas_equalTo(paidNewsView.mas_left).with.offset(15.0);
-            make.top.mas_equalTo(paidNewsView.mas_bottom).with.offset(- 90);
-            make.width.mas_equalTo(20.0);
-            make.height.mas_equalTo(20.0);
-        }];
+        avator.frame = CGRectMake(15, cellHeight - 90, 20, 20);
         avator.layer.cornerRadius = 10;
         avator.layer.masksToBounds = YES;
         [avator sd_setImageWithURL:[NSURL URLWithString:self.model.PaidNewsData[i][@"avatar"]]];
         
         UILabel *nickname = [[UILabel alloc] init];
         [paidNewsView addSubview:nickname];
-        [nickname mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.mas_equalTo(avator.mas_right).with.offset(10);
-            make.top.mas_equalTo(paidNewsView.mas_bottom).with.offset(-85);
-            make.right.mas_equalTo(paidNewsView.mas_right).with.offset(-30);
-            make.height.mas_equalTo(12);
-        }];
+        nickname.frame = CGRectMake(45, cellHeight - 85, cellWidth - 75, 12);
         nickname.textAlignment = NSTextAlignmentLeft;
         nickname.font = [UIFont boldSystemFontOfSize:12.0];
         nickname.textColor = [UIColor whiteColor];
@@ -114,12 +87,7 @@
         
         UILabel *updateInfo = [[UILabel alloc] init];
         [paidNewsView addSubview:updateInfo];
-        [updateInfo mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.mas_equalTo(paidNewsView.mas_left).with.offset(15.0);
-            make.top.mas_equalTo(paidNewsView.mas_bottom).with.offset(-50);
-            make.right.mas_equalTo(paidNewsView.mas_right).with.offset(-15.0);
-            make.height.mas_equalTo(12.0);
-        }];
+        updateInfo.frame = CGRectMake(15, cellHeight - 50, cellWidth - 30, 12);
         updateInfo.font = [UIFont systemFontOfSize:12.0];
         updateInfo.textColor = [UIColor whiteColor];
         updateInfo.textAlignment = NSTextAlignmentLeft;
